79f94c7d0a4670373274a912b48997f09780a2bb
kkoutsikos <k.koutsikos3@gmail.com>
2025-12-29 16:41:31 +0200

    client return_f_t = false

--- src/client.py
+++ src/client.py
@@ -36,9 +36,10 @@ class LocalLLMClient(ModelClient):
             "text-generation",
             model=self.model,
             tokenizer=self.tokenizer,
-            max_new_tokens=256,
+            max_new_tokens=512,
             do_sample=True, # Allow creativity for critiques
-            temperature=0.7
+            temperature=0.7,
+            return_full_text=False,
         )
 
     def call(self, api_kwargs={}, model_kwargs={}):
@@ -53,7 +54,7 @@ class LocalLLMClient(ModelClient):
         
         # Run Inference
         outputs = self.pipe(prompt)
-        return outputs[0]['generated_text'][len(prompt):] # Return only new text
+        return outputs[0]['generated_text']
 
 def get_client():
     """Singleton Accessor"""
